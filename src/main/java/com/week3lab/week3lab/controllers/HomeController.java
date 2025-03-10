a18bc5d2622ac75f344de63fe12aefaf32c780bc
kwasibaidoo <kwasi.baidoo@amalitech.com>
2025-03-10 15:11:18 +0000

    Removed request params

--- src/main/java/com/week3lab/week3lab/controllers/HomeController.java
+++ src/main/java/com/week3lab/week3lab/controllers/HomeController.java
@@ -2,14 +2,13 @@ package com.week3lab.week3lab.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public String welcome(@RequestParam String param) {
+    public String welcome() {
         return "Hello World, Kwasi Sakyi Baidoo: Deployed with AWS Elastic Beanstalk";
     }
     
